5a3f7d089735d90d0db1dbc8006ad3a886d075db
Juan-Pablo Silva <jpsilvapena@gmail.com>
2019-01-08 23:52:52 -0300

    Catch parsing errors

--- src/Parsing/src/main/java/parser/MethodCodeWriter.java
+++ src/Parsing/src/main/java/parser/MethodCodeWriter.java
@@ -1,6 +1,7 @@
 package parser;
 
 import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseProblemException;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.comments.Comment;
@@ -16,38 +17,56 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class MethodCodeWriter {
-    private final static String path = System.getProperty("user.dir") + "/src/main/java/parser/";
-    private final static String javaFilePath = path + "";
+    private final static String path = "E:\\Programacion\\CCXXXX - Research\\Resources\\corpus.tar\\";
+    private final static String javaFilePath = path + "java\\";
 
 
     public static void main(String[] args) throws IOException {
+        int progress = 0;
+        int errors = 0;
+
         try (Reader reader = Files.newBufferedReader(Paths.get(path + "db.csv"));
              CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
                      .withFirstRecordAsHeader()
                      .withIgnoreHeaderCase()
                      .withTrim());
-             Writer writer = Files.newBufferedWriter(Paths.get("method_db.csv"));
+             Writer writer = Files.newBufferedWriter(Paths.get(path + "method_db.csv"));
              CSVPrinter methodWriter = new CSVPrinter(writer, CSVFormat.DEFAULT
-                     .withHeader("File", "Doc", "Code"))) {
+                     .withHeader("file", "doc", "code"));
+             Writer errorFileWriter = Files.newBufferedWriter(Paths.get(path + "errors.txt"))) {
 
             for (CSVRecord line : csvParser) {
-                String fileHash = line.get("hash");
+                if (line.get("language").equals("java")) {
 
-                CompilationUnit code = JavaParser.parse(Paths.get(javaFilePath + fileHash));
-                for (MethodDeclaration method : code.findAll(MethodDeclaration.class)) {
-
-                    String javadoc = "";
-                    if (method.getJavadoc().isPresent()) {
-                        javadoc = method.getJavadoc().get().getDescription().toText().split("\\R", 2)[0].toLowerCase();
+                    if (progress % 39670 == 0) {
+                        System.out.println("Progress: " + progress / 39670 + "%, " + errors + " errors");
                     }
-                    method.removeJavaDocComment();
-                    method.getAllContainedComments().forEach(Comment::remove);
-                    String cleanedCode = method.toString().replaceAll("\\s+", " ");
 
-                    methodWriter.printRecord(fileHash, javadoc, cleanedCode);
+                    String fileHash = line.get("hash");
+
+                    try {
+                        CompilationUnit code = JavaParser.parse(Paths.get(javaFilePath + fileHash));
+                        for (MethodDeclaration method : code.findAll(MethodDeclaration.class)) {
+
+                            String javadoc = "";
+                            if (method.getJavadoc().isPresent()) {
+                                javadoc = method.getJavadoc().get().getDescription().toText().split("\\R", 2)[0].toLowerCase();
+                            }
+                            method.removeJavaDocComment();
+                            method.getAllContainedComments().forEach(Comment::remove);
+                            String cleanedCode = method.toString().replaceAll("\\s+", " ");
+
+                            methodWriter.printRecord(fileHash, javadoc, cleanedCode);
+                        }
+
+                        progress++;
+                    } catch (ParseProblemException e) {
+                        errors++;
+                        errorFileWriter.write(fileHash + "\n");
+                    }
+
                 }
             }
-
         }
     }
 }
